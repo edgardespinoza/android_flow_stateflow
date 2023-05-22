5af8f3933e0f747bfa024b58cd743a31b81fcd00
eespinor <edgard.espinoza.rivas@gmail.com>
2023-05-22 10:38:24 -0500

    chore: add flow

---
+++ app/src/main/java/com/eespinor/stateflow/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.eespinor.stateflow
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+
+class MainViewModel:ViewModel() {
+    val countDownFlow= flow<Int>{
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue>0){
+            delay(1000L)
+            currentValue--
+            emit(currentValue)
+        }
+    }
+}
